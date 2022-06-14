c66040796053bfd88f27ab1e58733db658fccf30
AdrianoCSG <107438077+AdrianoCSG@users.noreply.github.com>
2022-06-14 08:12:55 -0300

    Add files via upload

---
+++ fibonacci resolvido.py
@@ -0,0 +1,21 @@
+n = int(input("Termo que deseja encontrar:"))
+ultimo = 0
+penultimo = 1
+# comentario
+# cheio de ; num tem ; em python
+# ultimo estava declarado como 1, tinha que ser 0
+# não precisa desse if
+# input tava declarado como char e não int
+# não precisa printar o termo no final
+
+print("{}, {}".format(ultimo, penultimo), end = "")
+
+count = 3
+
+while count <= n:
+    termo = ultimo + penultimo
+    print(", {}".format(termo), end = "")
+    ultimo = penultimo
+    penultimo = termo
+    count += 1
+ 
